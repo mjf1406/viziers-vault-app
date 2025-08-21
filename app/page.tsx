3ba75dc25621aaeadf095c561aa15bce261f9ac8
mjf1406 <michael.fitzgerald.1406@gmail.com>
2025-08-21 20:48:16 +0900

    themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 /** @format */
 // app/page.tsx
 
+import { ThemeToggle } from "@/components/theme/theme-toggle";
 import TodosClient from "@/components/todos-client";
 
 export default function Page() {
     return (
         <div className="min-h-screen flex items-center justify-center">
+            <ThemeToggle />
             <TodosClient />
         </div>
     );

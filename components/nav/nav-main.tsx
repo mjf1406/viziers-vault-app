da41b94bd6a2a7bb793fa9e52f8022f7352295f1
mjf1406 <michael.fitzgerald.1406@gmail.com>
2025-08-30 15:28:20 +0900

    opacity change

--- components/nav/nav-main.tsx
+++ components/nav/nav-main.tsx
@@ -98,7 +98,7 @@ export function NavMain({ handleLinkClick }: NavMainProps) {
                                             className={cn(
                                                 "flex items-center px-3 py-2 rounded-md text-sm font-medium transition-colors",
                                                 isDisabled &&
-                                                    "opacity-50 cursor-not-allowed",
+                                                    "opacity-40 cursor-not-allowed",
                                                 !isDisabled &&
                                                     (isActive
                                                         ? "bg-muted text-primary"

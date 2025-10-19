543c972e774a8682feb4802628f641a8ae6b5f9e
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2025-10-19 14:21:23 +0900

    hrm

---
+++ app/(app)/app/parties/loading.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { Loader } from "@/components/nav/Loader";
+
+export default function LoadingPage() {
+    return (
+        <div>
+            <Loader text="Loading parties..." />
+        </div>
+    );
+}
